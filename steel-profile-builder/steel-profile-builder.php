3326673a077b1598cd9804ad20df1548c75eda8b
rjpdzzsdrt-ux <rjpdzzsdrt@privaterelay.appleid.com>
2026-01-21 13:47:29 +0200

    Add Steel Profiilid CPT.php

--- steel-profile-builder/steel-profile-builder.php
+++ steel-profile-builder/steel-profile-builder.php
@@ -10,3 +10,33 @@
 if (!defined('ABSPATH')) {
   exit;
 }
+
+class Steel_Profile_Builder {
+  const CPT = 'spb_profile';
+
+  public function __construct() {
+    add_action('init', [$this, 'register_cpt']);
+  }
+
+  public function register_cpt() {
+    register_post_type(self::CPT, [
+      'labels' => [
+        'name' => 'Steel Profiilid',
+        'singular_name' => 'Steel Profiil',
+        'add_new' => 'Lisa uus',
+        'add_new_item' => 'Lisa uus profiil',
+        'edit_item' => 'Muuda profiili',
+        'new_item' => 'Uus profiil',
+        'view_item' => 'Vaata profiili',
+        'search_items' => 'Otsi profiile',
+      ],
+      'public' => false,
+      'show_ui' => true,
+      'show_in_menu' => true,
+      'menu_icon' => 'dashicons-editor-kitchensink',
+      'supports' => ['title'],
+    ]);
+  }
+}
+
+new Steel_Profile_Builder();
